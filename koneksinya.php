6d1bd70e8d1021bcaf6bef41311b99418e0e0ef9
Dara Audia <86711239+daraaudia@users.noreply.github.com>
2021-07-01 17:25:20 +0700

    Create koneksinya.php

---
+++ koneksinya.php
@@ -0,0 +1,12 @@
+<?php
+
+	// cara untuk menghubungkan database ke PHP
+
+	$servernya		= "localhost";
+	$usernamenya	= "root";
+	$passwordnya	= "";
+	$nama_dbnya		= "proyekprpl";
+
+	$koneksinya		= mysqli_connect($servernya, $usernamenya, $passwordnya, $nama_dbnya);
+
+?>
